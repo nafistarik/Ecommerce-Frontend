ef1145c237ec38852bf028ced352ddcc89bbe032
nafistarik <nafistarik789@gmail.com>
2024-08-27 19:28:42 +0600

    item design changed

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,9 +6,13 @@ const Item = (props) => {
   return (
     <div className="item">
       <Link to={`/product/${props.id}`}>
-        <img onClick={window.scrollTo(0, 0)} src={props.image} alt="" />
+        <img
+          onClick={() => window.scrollTo(0, 0)}
+          src={props.image}
+          alt={props.name}
+        />
       </Link>
-      <p>{props.name}</p>
+      <p className="item-name">{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">${props.new_price}</div>
         <div className="item-price-old">${props.old_price}</div>
@@ -18,3 +22,4 @@ const Item = (props) => {
 };
 
 export default Item;
+
